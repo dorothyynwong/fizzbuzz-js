4889cf64f7606f639fb8cc781ee9b010c1d2aba6
Dorothy Wong <dorothy.wong@softwire.com>
2024-07-01 15:14:15 +0100

    FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,8 +6,16 @@ function fizzbuzz() {
     console.log('Welcome to FizzBuzz!');
 
     // Put your code here...
+    let result = "";
+    for(let i=1; i<=100; i++) {
+        let str = "";
+        if (i%3 === 0) str += "Fizz";
+        if (i%5 === 0) str += "Buzz";
+        if (i%3 !== 0 && i%5!==0) str += i.toString();
+        result += str + " ";
+    }
 
-    
+    console.log(result);
 }
 
 // Now we run the main function...
